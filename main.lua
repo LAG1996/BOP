b4f313722b18f16bacc3f2554cb006499ffdc626
LAG1996 <luis.a.garcia01996@gmail.com>
2017-04-22 12:16:23 -0500

    Drawing the Graphics is Almost Fully Operational
    TODO:
    -COLORS FOR NODES
    -MAKING SURE NO NODES ARE OFF SCREEN
    -DRAWING PNG
    -GAME LOGIC: This includes, point system, AI, timer, losing state

--- main.lua
+++ main.lua
@@ -1,7 +1,120 @@
+HC = require 'HC'
+
+--array to hold rectangle information
+arr_rectangles = {}
+--array to hold rectangle hitbox information
+arr_hitboxes = {}
+
+MAX_RECTS = 49
+
+WINDOW_WIDTH = 650
+WINDOW_HEIGHT = 700
+
+RECT_SIDE = WINDOW_WIDTH/10
+RECT_OFFSET = 10.0
+
+MAX_RECTS_ACROSS = MAX_RECTS/2
+MAX_RECTS_VERTICAL = MAX_RECTS/2
+
+left_most_rect = {}
+bottom_most_rect = {}
+
+first_rectangle = {}
+first_hitbox = {}
+
+function love.load()
+
+	love.graphics.setBackgroundColor(50, 50, 50)
+	--create a random seed
+	math.randomseed(os.time())
+	--initialize all the rectangles and their hitboxes
+	first_rectangle = {}
+	first_rectangle["x"] = math.random(MAX_RECTS)
+	first_rectangle["y"] = math.random(MAX_RECTS)
+	first_rectangle["width"] = RECT_SIDE
+	first_rectangle["height"] = RECT_SIDE
+
+	first_hitbox = {}
+	first_hitbox["x"] = first_rectangle["x"]
+	first_hitbox["y"] = first_rectangle["y"]
+	first_hitbox["width"] = first_rectangle["width"]
+	first_hitbox["height"] = first_rectangle["height"]
+
+	open_adjacent_spots = {{first_rectangle["x"] + RECT_SIDE + RECT_OFFSET, first_rectangle["y"]}, {first_rectangle["x"] - (RECT_SIDE + RECT_OFFSET), first_rectangle["y"]}, {first_rectangle["x"], first_rectangle["y"] + RECT_SIDE + RECT_OFFSET}, {first_rectangle["x"], first_rectangle["y"] - (RECT_SIDE + RECT_OFFSET)}}
+
+	x = first_rectangle["x"] - math.floor(MAX_RECTS/2)
+	y = first_rectangle["y"] - math.floor(MAX_RECTS/2)
+
+	--Create a map for occupied areas
+	occupied_map = {}
+	while x < first_rectangle["x"] + math.floor(MAX_RECTS/2) do
+		while y < first_rectangle["y"] + math.floor(MAX_RECTS/2) do
+			occupied_map[x .. "," .. y] = false 
+			y = y + RECT_SIDE + RECT_OFFSET
+		end
+		x = x + RECT_SIDE + RECT_OFFSET
+	end
+
+	occupied_map[first_rectangle["x"] .. "," .. first_rectangle["y"]] = true --Set the first graph as occupied
+
+	left_most_rect = first_rectangle
+	bottom_most_rect = first_rectangle
+
+	
+	i = 2
+	while i <= MAX_RECTS do
+		--create a random seed
+		math.randomseed(os.time())
+		open_index = math.random(table.getn(open_adjacent_spots))
+
+		new_rectangle = {}
+		new_rectangle["x"] = open_adjacent_spots[open_index][1]
+		new_rectangle["y"] = open_adjacent_spots[open_index][2]
+		new_rectangle["width"] = RECT_SIDE
+		new_rectangle["height"] = RECT_SIDE
+
+		new_hitbox = {}
+
+		new_hitbox["x"] = new_rectangle["x"]
+		new_hitbox["y"] = new_rectangle["y"]
+		new_hitbox["width"] = new_rectangle["width"]
+		new_hitbox["height"] = new_rectangle["height"]
+
+
+		table.insert(open_adjacent_spots, {new_rectangle["x"] - (RECT_SIDE + RECT_OFFSET), new_rectangle["y"]})
+		table.insert(open_adjacent_spots, {new_rectangle["x"] + RECT_SIDE + RECT_OFFSET, new_rectangle["y"]})
+		table.insert(open_adjacent_spots, {new_rectangle["x"], new_rectangle["y"] - (RECT_SIDE + RECT_OFFSET)})
+		table.insert(open_adjacent_spots, {new_rectangle["x"], new_rectangle["y"] + RECT_SIDE + RECT_OFFSET})
+
+		occupied_map[new_rectangle["x"] .. "," .. new_rectangle["y"]] = true
+
+		if new_rectangle["x"] < left_most_rect["x"] then
+			left_most_rect = new_rectangle
+		end
+		if new_rectangle["y"] < bottom_most_rect["y"] then
+			bottom_most_rect = new_rectangle
+		end
+
+
+		table.remove(open_adjacent_spots, open_index) --remove this open index so that it's not considered again
+
+		table.insert(arr_rectangles, new_rectangle)
+		table.insert(arr_hitboxes, new_hitbox)
+
+		i = i + 1
+	end
+
+end
+
+function love.update(dt)
+end
 
 function love.draw(dt)
-	love.graphics.print('Hello World!', 400, 300)
-end
-function love.update(dt)
+	OFFSET_X = left_most_rect["x"]
+	OFFSET_Y = -1*bottom_most_rect["x"]
 
+    for i, v in ipairs(arr_rectangles) do
+    	love.graphics.print(v["x"] - OFFSET_X .. ", " .. v["y"] + OFFSET_Y, 10, i * 15)
+    	love.graphics.rectangle("fill", v["x"] - OFFSET_X, v["y"] - OFFSET_Y, v["width"], v["height"], 20, 20)
+    end
 end
